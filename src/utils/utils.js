c0199feb0b6830e47ee01864fd664d8d3d6e69e0
iz-Brum <izann.brum@alunosenai.mt>
2024-07-25 09:24:22 -0400

    Substituir detailLabels por headerDictionary e ajustar formatações em utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -37,20 +37,54 @@ export const detailLabels = {
     qualidadedaagua: "Qualidade da Água",
     tanqueevapo: "Tanque Evaporimétrico"
 };
-  
+
+// Mapeamento dos rótulos para os cabeçalhos das tabelas
+export const headerDictionary = {
+    stationCode: "CÓDIGO DA ESTAÇÃO",
+    nome: "NOME",
+    bacia: "BACIA",
+    subbacia: "SUB-BACIA",
+    rio: "RIO",
+    estado: "ESTADO",
+    municipio: "MUNICÍPIO",
+    responsavel: "RESPONSÁVEL",
+    operadora: "OPERADORA",
+    latitude: "LATITUDE",
+    longitude: "LONGITUDE",
+    areadrenagem: "ÁREA DE DRENAGEM",
+    tipoestacao: "TIPO DE ESTAÇÃO",
+    operando: "OPERANDO",
+    telemetrica: "TELEMETRIA",
+    climatologica: "CLIMATOLÓGICA",
+    pluviometro: "PLUVIÔMETRO",
+    registradorchuva: "REGISTRADOR DE CHUVA",
+    escala: "ESCALA",
+    registradornivel: "REGISTRADOR DE NÍVEL",
+    descargaliquida: "DESCARGA LÍQUIDA",
+    sedimentos: "SEDIMENTOS",
+    qualidadedaagua: "QUALIDADE DA ÁGUA",
+    tanqueevapo: "TANQUE EVAPORIMÉTRICO",
+    data: "DATA",
+    chuva: "CHUVA (MM)",
+    nivel: "NÍVEL (CM)",
+    vazao: "VAZÃO (M³/S)",
+    'soma_ult_leituras': "PERÍODO",
+    sum_chuva: "SOMA DA CHUVA (MM)"
+};
+
 // Função para formatar as datas no horário local do usuário
 export const formatDate = (dateString) => {
+    if (!dateString) return 'Data inválida';  // Verifica se a data é nula ou indefinida
     const date = new Date(dateString);
     const localTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    const formattedDate = date.toLocaleString('pt-BR', { timeZone: localTimeZone });
-    console.log(`Original: ${dateString}, UTC Date: ${date.toISOString()}, Local Time Zone: ${localTimeZone}, Formatted: ${formattedDate}`);
-    return formattedDate;
+    return date.toLocaleString('pt-BR', { timeZone: localTimeZone });
 };
-  
+
 // Função para formatar os números
 export const formatNumber = (number) => {
+    if (number === null || number === undefined) return 'N/A';  // Trata valores nulos ou indefinidos
     if (number % 1 === 0) {
-      return number.toLocaleString('pt-BR'); // Formata números inteiros
+        return number.toLocaleString('pt-BR'); // Formata números inteiros
     }
     return number.toLocaleString('pt-BR', { minimumFractionDigits: 1 }); // Formata números decimais
 };
