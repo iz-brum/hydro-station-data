32ca1e2204a92f4e319f3b069c8f105ac4e81077
iz-Brum <izann.brum@alunosenai.mt>
2024-07-27 14:48:18 -0400

    Update App.js
    Ajuste das rotas e basename no App.js
    
    1. Alteração do `basename` no componente `Router` para `/hydro-station-data` para refletir a URL base correta do projeto.
    2. Ajuste das rotas de navegação:
       - Mudança da rota de `hydroHome` para a raiz `/` no `Link` e `Route`.
       - Atualização dos links de navegação para refletir as mudanças nas rotas.

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import AllHydroDataPage from './components/AllHydroDataPage';
 import StationDetailsPage from './components/StationDetailsPage';
 import { LoadingProvider, useLoading } from './context/LoadingContext';
 import Loading from './components/Loading/Loading';
-import './App.css'; 
+import './App.css';
 
 const App = () => {
   return (
     <LoadingProvider>
-      <Router>
+      <Router basename="/hydro-station-data">
         <nav>
           <ul>
-            <li><Link to="/hydroHome">INÍCIO</Link></li>
+            <li><Link to="/">INÍCIO</Link></li>
             <li><Link to="/data-input">PESQUISAR ESTAÇÕES</Link></li>
           </ul>
         </nav>
         <Routes>
-          <Route path="/hydroHome" exact element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/data-input" element={<DataInputPage />} />
           <Route path="/all-hydro-data" element={<AllHydroDataPage />} />
           <Route path="/station-details" element={<StationDetailsPage />} />
