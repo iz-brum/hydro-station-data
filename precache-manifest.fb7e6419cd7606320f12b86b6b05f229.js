901051a9a32b4fce7083c89dfc68f4a1e11c8da1
iz-Brum <izann.brum@alunosenai.mt>
2024-07-26 23:59:30 -0400

    Updates

---
+++ precache-manifest.fb7e6419cd7606320f12b86b6b05f229.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1565bceee8d24c0aef948bee707b7b89",
+    "url": "/hydro-station-data/index.html"
+  },
+  {
+    "revision": "4cd727957c9b026d6205",
+    "url": "/hydro-station-data/static/css/main.9711150a.chunk.css"
+  },
+  {
+    "revision": "df4c35b4a219f6629bca",
+    "url": "/hydro-station-data/static/js/2.b92849ed.chunk.js"
+  },
+  {
+    "revision": "4cd727957c9b026d6205",
+    "url": "/hydro-station-data/static/js/main.b2abed54.chunk.js"
+  },
+  {
+    "revision": "b485674baeedc7295502",
+    "url": "/hydro-station-data/static/js/runtime~main.79ef87fe.js"
+  }
+]);
