7575abcc5f584be3ed328eebdfc39dcad8dd754e
iz-Brum <izann.brum@alunosenai.mt>
2024-07-26 22:39:12 -0400

    Melhorias na exibição de dados e ajustes na lógica de PreviewModal.js
    - Adicionado `selectedRainSummary` como propriedade ao componente `PreviewModal` para suportar a pré-visualização de resumos de chuvas.
    - Corrigido mapeamento de colunas para incluir `PERÍODO` e usar corretamente `mapPeriodLabel` para exibir rótulos adequados.
    - Ajustada lógica de filtragem de dados para incluir novas categorias como `Resumo chuvas` e garantir que apenas dados selecionados sejam exibidos.
    - Otimizada a verificação de colunas selecionadas para cada categoria de dados, garantindo uma exibição precisa dos dados na pré-visualização.
    - Melhorada a organização do código para facilitar futuras manutenções e adições de funcionalidades.
    
    Essas mudanças melhoram a precisão e a clareza da pré-visualização de dados, garantindo que os usuários vejam as informações corretas antes de confirmar a exportação.

--- src/components/PreviewModal.js
+++ src/components/PreviewModal.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './css/PreviewModal.css';
 import { mapPeriodLabel, headerDictionary, formatDate, formatNumber, detailLabels } from '../utils/utils';
 
-const PreviewModal = ({ data, onConfirm, onCancel, selectedDetails = {}, selectedHydro24h = {} }) => {
+const PreviewModal = ({ data, onConfirm, onCancel, selectedDetails = {}, selectedHydro24h = {}, selectedRainSummary = {} }) => {
   console.log('Dados para pré-visualização:', data);
 
   if (!data) {
@@ -25,14 +25,16 @@ const PreviewModal = ({ data, onConfirm, onCancel, selectedDetails = {}, selecte
             <tr key={index}>
               {columns.map((col, colIndex) => (
                 <td key={colIndex}>
-                  {col === 'soma_ult_leituras'
-                    ? mapPeriodLabel(row[col])
-                    : col === 'data'
-                      ? formatDate(row[col])
-                      : row[col] !== null && row[col] !== undefined
-                        ? formatNumber(row[col])
-                        : 'N/A'}
-                </td>
+                {col === 'soma_ult_leituras'
+                  ? mapPeriodLabel(row[col])
+                  : col === 'data'
+                  ? formatDate(row[col])
+                  : col === 'PERÍODO'
+                  ? row['period'] // Usando 'period' para acessar os dados corretos
+                  : row[col] !== null && row[col] !== undefined
+                  ? formatNumber(row[col])
+                  : 'N/A'}
+              </td>
               ))}
             </tr>
           ))}
@@ -43,20 +45,20 @@ const PreviewModal = ({ data, onConfirm, onCancel, selectedDetails = {}, selecte
 
   const detalhesData = data.filter(d => d.category === 'Detalhes');
   const hidro24hData = data.filter(d => d.category === 'Hidro 24h');
-  const chuvaUltData = data.filter(d => d.category === 'Chuva Últ');
+  const chuvaUltData = data.filter(d => d.category === 'Resumo chuvas');
 
   const selectedDetailColumns = Object.keys(selectedDetails).filter(key => selectedDetails[key]);
   const selectedHydrodataColumns = Object.keys(selectedHydro24h).filter(key => selectedHydro24h[key]);
-  const selectedRainSummaryColumns = ['stationCode', 'period', 'sum_chuva'];
+  const selectedRainSummaryColumns = ['stationCode', 'PERÍODO', 'sum_chuva'];
 
   return (
     <div className="preview-modal-overlay">
       <div className="preview-modal">
         <h3>Pré-visualização dos Dados</h3>
         <div className="preview-table">
-          {detalhesData.length > 0 && renderTable('Ficha das estações', detalhesData, ['stationCode', ...selectedDetailColumns], true)}
-          {chuvaUltData.length > 0 && renderTable('Resumo de chuvas', chuvaUltData, selectedRainSummaryColumns)}
-          {hidro24hData.length > 0 && renderTable('Histórico 24h', hidro24hData, ['stationCode', ...selectedHydrodataColumns])}
+          {selectedDetailColumns.length > 0 && detalhesData.length > 0 && renderTable('Ficha das estações', detalhesData, ['stationCode', ...selectedDetailColumns], true)}
+          {selectedRainSummaryColumns.length > 0 && chuvaUltData.length > 0 && renderTable('Resumo de chuvas', chuvaUltData, selectedRainSummaryColumns)}
+          {selectedHydrodataColumns.length > 0 && hidro24hData.length > 0 && renderTable('Histórico 24h', hidro24hData, ['stationCode', ...selectedHydrodataColumns])}
         </div>
         <div className="button-container">
           <button className="confirm-button" onClick={onConfirm}>Confirmar Download</button>
