4e42c95ee31a586e346d728ac7c1375679cb158d
iz-Brum <izann.brum@alunosenai.mt>
2024-07-28 15:03:33 -0400

    Updates

--- precache-manifest.046a15c7c213157eff9927ba4928cb49.js
+++ precache-manifest.046a15c7c213157eff9927ba4928cb49.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b157761baea70fefb8f13ff0342e0ed3",
+    "revision": "16ca402c49d75134eca80cdeebbc1367",
     "url": "/hydro-station-data/index.html"
   },
   {
-    "revision": "bf289fcef3ffda459165",
+    "revision": "a1d3a0b1cac045dc7834",
     "url": "/hydro-station-data/static/css/main.9711150a.chunk.css"
   },
   {
-    "revision": "df4c35b4a219f6629bca",
-    "url": "/hydro-station-data/static/js/2.b92849ed.chunk.js"
+    "revision": "0028d39d859148e55656",
+    "url": "/hydro-station-data/static/js/2.29bf1e5c.chunk.js"
   },
   {
-    "revision": "bf289fcef3ffda459165",
-    "url": "/hydro-station-data/static/js/main.c624dab4.chunk.js"
+    "revision": "a1d3a0b1cac045dc7834",
+    "url": "/hydro-station-data/static/js/main.772ac940.chunk.js"
   },
   {
     "revision": "b485674baeedc7295502",
