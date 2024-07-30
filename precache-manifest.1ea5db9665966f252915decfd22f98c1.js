7e8d392cd394ad1003d4f55cff87483c51665712
iz-Brum <izann.brum@alunosenai.mt>
2024-07-29 21:35:01 -0400

    Updates

---
+++ precache-manifest.1ea5db9665966f252915decfd22f98c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14933f3060086fda0e4ee31fdad4cd79",
+    "url": "/hydro-station-data/index.html"
+  },
+  {
+    "revision": "4b506673fb87e1739ebc",
+    "url": "/hydro-station-data/static/css/main.0bcf0bf2.chunk.css"
+  },
+  {
+    "revision": "63c461c9409ba3373908",
+    "url": "/hydro-station-data/static/js/2.2db57015.chunk.js"
+  },
+  {
+    "revision": "9343bcc0e8f1282787bd9871dfd7298b",
+    "url": "/hydro-station-data/static/js/2.2db57015.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b506673fb87e1739ebc",
+    "url": "/hydro-station-data/static/js/main.d959b760.chunk.js"
+  },
+  {
+    "revision": "a67a662222389e668047",
+    "url": "/hydro-station-data/static/js/runtime-main.32214611.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hydro-station-data/precache-manifest.36035933ac3bdfd45573bdf58ce88e35.js"
+  "/hydro-station-data/precache-manifest.1ea5db9665966f252915decfd22f98c1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hydro-station-data/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
