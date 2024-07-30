40c1ca6eb677c2f2c75fb7f87de03236aad5e1dd
iz-Brum <izann.brum@alunosenai.mt>
2024-07-29 21:12:18 -0400

    Refatoração no AllHydroDataPage.js
    - Importações: Ajustadas as importações para incluir funções utilitárias e estilos, além dos componentes necessários do Recharts.
    - Mapeamento de Dados: Adicionada a constante `chartData` para mapear os dados dos gráficos, incluindo parsing e formatação.
    - Gráficos: Implementados gráficos para dados de chuva, nível e vazão utilizando `ResponsiveContainer` e `LineChart`.

--- src/components/AllHydroDataPage.js
+++ src/components/AllHydroDataPage.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { formatNumber, formatDate } from '../utils/utils'; // Importa os utilitários
-import './css/DataView.css';  // Importa o novo CSS para os cartões de estação
+import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid, ResponsiveContainer } from 'recharts';
+import { formatDate, getDomain } from '../utils/utils';
+import './css/DataView.css';
 
-// Componente para exibir todos os dados hidrométricos
 const AllHydroDataPage = () => {
     const location = useLocation();
     const navigate = useNavigate();
@@ -13,6 +13,13 @@ const AllHydroDataPage = () => {
         return <div>Nenhum dado disponível.</div>;
     }
 
+    const chartData = data.map(item => ({
+        data: formatDate(item.data),
+        chuva: parseFloat(item.chuva),
+        nivel: parseFloat(item.nivel),
+        vazao: parseFloat(item.vazao),
+    }));
+  
     return (
         <div className="all-hydro-data-container">
             <div className="station-header">
@@ -22,27 +29,50 @@ const AllHydroDataPage = () => {
                 </div>
                 <h3 style={{ margin: '0' }}>Dados Hidrométricos 24h</h3>
             </div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Data</th>
-                        <th>Chuva (mm)</th>
-                        <th>Nível (cm)</th>
-                        <th>Vazão (m³/s)</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map((item, index) => (
-                        <tr key={index}>
-                            <td>{formatDate(item.data)}</td>
-                            <td>{formatNumber(item.chuva)}</td>
-                            <td>{formatNumber(item.nivel)}</td>
-                            <td>{formatNumber(item.vazao)}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-            <button onClick={() => navigate(-1)}>Voltar</button>
+
+            {/* Gráfico de Chuva */}
+            <div className="chart-container">
+                <h4>Chuva (mm)</h4>
+                <ResponsiveContainer width="100%" height={300}>
+                    <LineChart data={chartData.slice().reverse()} margin={{ left: 10, right: 20 }}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="data" tickFormatter={(tick) => new Date(tick.replace(/(\d{2})\/(\d{2})\/(\d{4}),/, '$2/$1/$3')).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })} />
+                        <YAxis domain={getDomain(chartData,'chuva')} />
+                        <Tooltip formatter={(value) => `${value} mm`} />
+                        <Line type="monotone" dataKey="chuva" stroke="#8884d8" dot={true} />
+                    </LineChart>
+                </ResponsiveContainer>
+            </div>
+
+            {/* Gráfico de Nível */}
+            <div className="chart-container">
+                <h4>Nível (cm)</h4>
+                <ResponsiveContainer width="100%" height={300}>
+                    <LineChart data={chartData.slice().reverse()} margin={{ left: 25, right: 20 }}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="data" tickFormatter={(tick) => new Date(tick.replace(/(\d{2})\/(\d{2})\/(\d{4}),/, '$2/$1/$3')).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })} />
+                        <YAxis domain={getDomain('nivel')} />
+                        <Tooltip formatter={(value) => `${value} cm`} />
+                        <Line type="monotone" dataKey="nivel" stroke="#82ca9d" dot={true} />
+                    </LineChart>
+                </ResponsiveContainer>
+            </div>
+
+            {/* Gráfico de Vazão */}
+            <div className="chart-container">
+                <h4>Vazão (m³/s)</h4>
+                <ResponsiveContainer width="100%" height={300}>
+                    <LineChart data={chartData.slice().reverse()} margin={{ left: 30, right: 20 }}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="data" tickFormatter={(tick) => new Date(tick.replace(/(\d{2})\/(\d{2})\/(\d{4}),/, '$2/$1/$3')).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })} />
+                        <YAxis domain={getDomain('vazao')} />
+                        <Tooltip formatter={(value) => `${value} m³/s`} />
+                        <Line type="monotone" dataKey="vazao" stroke="#ffc658" dot={true} />
+                    </LineChart>
+                </ResponsiveContainer>
+            </div>
+            
+            <button className='btn-voltar' onClick={() => navigate(-1)}>Voltar</button>
         </div>
     );
 };
