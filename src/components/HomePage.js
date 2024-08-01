2a47d0d568ddc4fe15b7bc496e1dd59fe8f1adc6
iz-Brum <izann.brum@alunosenai.mt>
2024-07-31 20:05:29 -0400

    Update HomePage.js
    Alterações em HomePage.js
    
    - Estilos e Classes CSS
      - Ajustes no componente de navegação.
      - Alteração de div para main para melhorar a semântica do HTML.
    
    - Home Content:
      - Modificação de `<div className="home-content">` para `<main className="home-content">`.
      - Ajuste na estrutura do conteúdo para melhor semântica e acessibilidade.
      - Substituição de tags `<div>` por tags `<main>`.
    
    - Texto e Links:
      - Atualização do conteúdo textual para melhor clareza e concisão.
      - Inclusão de novos links e botões estilizados com classes CSS.
      - Melhoria na acessibilidade dos links e botões.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,3 @@
-// src/components/HomePage.js
-
 import React, { useEffect } from 'react';
 import './css/HomePage.css';
 import { useLoading } from '../context/LoadingContext';
@@ -20,17 +18,11 @@ const HomePage = () => {
         <p>Informações hidrológicas e meteorológicas ao seu alcance.</p>
       </header>
 
-      <div className="home-content">
+      <main className="home-content">
         <section className="home-section">
           <h3>Sobre o Aplicativo</h3>
           <p>
-            O aplicativo HidroWeb mobile é uma ferramenta integrante do Sistema Nacional de Informações sobre Recursos Hídricos (SNIRH), gerenciado pela Agência Nacional de Águas (ANA), e permite o acesso aos dados telemétricos coletados pela Rede Hidrometeorológica Nacional (RHN), reunindo dados de chuvas, níveis e vazões de rios, além de informações sobre vazões afluentes, defluentes, níveis e volumes de vários reservatórios em todo o território nacional.
-          </p>
-          <p>
-            Trata-se de uma importante ferramenta para a sociedade e instituições públicas e privadas, pois os dados de reservatórios e aqueles coletados pelas estações hidrometeorológicas são imprescindíveis para a gestão dos recursos hídricos e em diversas atividades econômicas, como geração de energia, irrigação, navegação e indústria, além do projeto, manutenção e operação de infraestrutura hidráulica de pequeno e grande porte, como barragens, drenagem pluvial urbana e mesmo bueiros e telhados.
-          </p>
-          <p>
-            Por meio dessas informações, pode-se, ainda, acompanhar a ocorrência de eventos hidrológicos considerados críticos, inundações e secas, e se planejar medidas de mitigação dos impactos decorrente desses eventos. Mais que acompanhar esses fenômenos, o conjunto de dados até hoje coletados no âmbito da RHN também permite, em diversos casos, a simulação e previsão de eventos hidrológicos e de seus impactos em áreas urbanas ou rurais em diversas bacias hidrográficas.
+            O aplicativo HidroWeb mobile é uma ferramenta integrante do Sistema Nacional de Informações sobre Recursos Hídricos (SNIRH), gerenciado pela Agência Nacional de Águas (ANA), e permite o acesso aos dados telemétricos coletados pela Rede Hidrometeorológica Nacional (RHN)...
           </p>
         </section>
         <section className="home-section">
@@ -42,20 +34,21 @@ const HomePage = () => {
             <li>Mapas interativos para localização das estações.</li>
           </ul>
         </section>
-        <section className="home-section">
-          <h3>Como Utilizar</h3>
-          <p>
-            Navegue pelo aplicativo para explorar as diferentes funcionalidades oferecidas. Utilize o menu principal para acessar seções específicas como "Estatísticas", "Alertas", "Mapas" e muito mais.
-          </p>
-          <a href="https://www.snirh.gov.br" target="_blank" rel="noopener noreferrer">Saiba mais</a>
+        <section className="home-section flex-section">
+          <div className="text-content">
+            <h3>Como Utilizar</h3>
+            <p>
+              Navegue pelo aplicativo para explorar as diferentes funcionalidades oferecidas. Utilize o menu principal para acessar seções específicas como "Estatísticas", "Alertas", "Mapas" e muito mais.
+            </p>
+            <a href="https://www.snirh.gov.br" target="_blank" rel="noopener noreferrer" className="cta-button">Saiba mais</a>
+          </div>
+          <div className="home-gif-container">
+            <img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMHNpcDM4cWNwZ28zeGZjcTIxMDZtNjI5ODhneGRpcGN6aXdqbW5lZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3oKIPEqDGUULpEU0aQ/giphy.webp" alt="Descrição do GIF" className="home-gif" />
+            <p className="home-gif-credit"><a href="https://giphy.com/gifs/cartoon-character-2d-3oKIPEqDGUULpEU0aQ" target="_blank" rel="noopener noreferrer">via GIPHY</a></p>
+          </div>
         </section>
-      </div>
+      </main>
       <footer className="home-footer">
-        <div className="footer-socials">
-          <a href="#facebook">Facebook</a>
-          <a href="#twitter">Twitter</a>
-          <a href="#instagram">Instagram</a>
-        </div>
         <p>&copy; 2024 HidroInfo. Todos os direitos reservados.</p>
       </footer>
     </div>
