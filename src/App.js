9a21930dad14952e4ca00f25636776bd58fc68be
iz-Brum <izann.brum@alunosenai.mt>
2024-08-01 22:57:20 -0400

    Update App.js
    - Removido basename do componente Router:
      - O código condicional que define basename baseado no ambiente de desenvolvimento foi removido.
      - O atributo basename no Router foi excluído.
    
    - Atualização dos links de navegação:
      - Atualizados os componentes NavLink para não utilizarem mais a função isActive.
      - Adicionados novos elementos NavLink com o novo estilo e lógica simplificada.

--- src/App.js
+++ src/App.js
@@ -9,15 +9,21 @@ import Loading from './components/Loading/Loading';
 import './App.css';
 
 const App = () => {
-  const basename = process.env.NODE_ENV === 'development' ? '' : '/hydro-station-data';
-
   return (
     <LoadingProvider>
-      <Router basename={basename}>
+      <Router>
         <nav className="navbar">
           <ul className="nav-list">
-            <li className="nav-item"><NavLink to="/" end className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>INÍCIO</NavLink></li>
-            <li className="nav-item"><NavLink to="/data-input" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>PESQUISAR ESTAÇÕES</NavLink></li>
+            <li className="nav-item">
+              <NavLink to="/" end className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>
+                INÍCIO
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/data-input" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>
+                PESQUISAR ESTAÇÕES
+              </NavLink>
+            </li>
           </ul>
         </nav>
         <Routes>
